5d1d068bcc6f204d1e1b1f7d97936806ac51ebb2
Pato Ezequiel <pato.ezequiel@gmail.com>
2019-05-17 00:47:59 -0300

    Crear reglas de TSLint y ajustar archivos a ellas

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller( )
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	public constructor(
+		private readonly appService: AppService,
+	) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+	@Get( )
+	public getHello( ): string {
+		return this.appService.getHello( );
+	}
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
+@Injectable( )
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+
+	public getHello( ): string {
+		return 'Hello World!';
+	}
+
 }
