9300f814bc7ceff8efe7707fad1b9843963f469a
Pato Ezequiel <pato.ezequiel@gmail.com>
2019-05-17 01:27:20 -0300

    Agregar log al iniciar servidor con éxito

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const port = 3000;
+const puerto = 3000;
 
 async function bootstrap( ) {
 	const app = await NestFactory.create( AppModule );
-	await app.listen( port );
+	await app.listen( puerto, ( ) => {
+		// tslint:disable-next-line: no-console
+		console.log( `\nEl servidor está escuchando peticiones HTTP en http://localhost:${ puerto }\n` );
+	});
 }
 
 bootstrap( );
